6881c86a10d11e4369506df43220f0fda0ec79cb
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-03-17 23:07:31 +0200

    ......

--- src/ConverterTest.java
+++ src/ConverterTest.java
@@ -1,16 +1,16 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ConverterTest {
-  Converter converter = new Converter();
-    @Test
-    void main() {
-
-    }
-
-    @Test
-    void create() {
-        Converter.tony();
-    }
-}
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class ConverterTest {
+//  Converter converter = new Converter();
+//    @Test
+//    void main() {
+//
+//    }
+//
+//    @Test
+//    void create() {
+//        Converter.tony();
+//    }
+//}
