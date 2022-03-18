886a6c7178b872ffe78b6d5688e42263489159e2
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2022-03-18 18:15:27 +0200

    testcases

--- src/ConverterTest.java
+++ src/ConverterTest.java
@@ -1,25 +1,21 @@
-/*import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class ConverterTest {
 
-//    @Test
-//    void GeneratingExpression() {
-//        assertAll(
-//                ()-> assertEquals("<img src=\"https://www.w3schools.com/html/pic_trulli.jpg\" />",Converter.parse("ADD IMAGE WITH SOURCE \"https://www.w3schools.com/html/pic_trulli.jpg\""))
-////                ()-> assertEquals(true,Converter.validate("ADD HEADING WITH TEXT \"Hello World\"")),
-////                ()-> assertEquals(true,Converter.validate("ADD PARAGRAPH WITH TEXT \"Welcome\" AND WITH FONT \"Arial\" AND WITH COLOR \"Red\"")),
-////                ()-> assertEquals(true,Converter.validate("ADD LINK WITH TEXT \"Search\" AND WITH LINK \"http://google.com")),
-////                ()-> assertEquals(true,Converter.validate("ADD LINK WITH TEXT \"Email\" AND WITH LINK \"http://gmail.com\" AND WITH COLOR \"Green")),
-////                ()-> assertEquals(true,Converter.validate("ADD PARAGRAPH WITH FONT \"Arial\" AND WITH TEXT \"Good bye\"")),
-////                ()-> assertEquals(false,Converter.validate("ADDD PARAGRAPH WITH FONT \"Calibri\" AND TEXT \"Hello\"")),
-////                ()-> assertEquals(false,Converter.validate("ADD CANVAS WITH COLOR \"Red\"")),
-////                ()-> assertEquals(false,Converter.validate("ADD PARAGRAPH COLOR \"Red\" TEXT \"Hello\"")),
-////                ()-> assertEquals(false,Converter.validate("ADD HEADING "))
-//        );
-//    }
+    @Test
+   void GeneratingExpression() {
+       assertAll(
+                ()-> assertEquals("<img src=\"https://www.w3schools.com/html/pic_trulli.jpg\" />",Converter.parse("ADD IMAGE WITH SOURCE \"https://www.w3schools.com/html/pic_trulli.jpg\"")),
+               ()-> assertEquals("<h1>Hello World</h1>",Converter.parse("ADD HEADING WITH TEXT \"Hello World\"")),
+               ()-> assertEquals("<p style=\"font-family:Arial;color:Red;\">Welcome</p>",Converter.parse("ADD PARAGRAPH WITH TEXT \"Welcome\" AND WITH FONT \"Arial\" AND WITH COLOR \"Red\"")),
+               ()-> assertEquals("<a href=\"http://google.com\">Search</a>",Converter.parse("ADD LINK WITH TEXT \"Search\" AND WITH LINK \"http://google.com")),
+               ()-> assertEquals("<a href=\"http://gmail.com\" style=\"color:Green;\">Email</a>",Converter.parse("ADD LINK WITH TEXT \"Email\" AND WITH LINK \"http://gmail.com\" AND WITH COLOR \"Green"))
+
+      );
+    }
 
     @Test
     void main() throws Exception {
@@ -36,4 +32,4 @@ class ConverterTest {
               ()-> assertEquals(false,Converter.validate("ADD HEADING "))
               );
     }
-}*/
+}
